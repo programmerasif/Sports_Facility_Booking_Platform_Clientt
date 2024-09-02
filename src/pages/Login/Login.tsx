c32d551bf49fddefcda82ba59ee6e41cd192a23c
Asif Khan <mdasifkhan3757@gmail.com>
2024-09-02 01:46:42 -0700

    login page redesign

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -3,13 +3,14 @@ import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { BottomGradient } from "@/components/ui/BottomGradient";
 import { LabelInputContainer } from "@/components/ui/LabelInputContainer";
-import login from "../../assets/login2.png";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { FormValues } from "@/types/types";
 import { useLoginMutation } from "@/redux/feature/auth/authApi";
 import { NavLink } from "react-router-dom";
 import { setUserInfo } from "@/redux/feature/userInfo/userInfoSlice";
 import { useAppDispatch } from "@/redux/api/hook";
+import Lottie from "lottie-react";
+import login from '../../assets/login.json'
 
 
 const Login: React.FC = () => {
@@ -49,8 +50,15 @@ const Login: React.FC = () => {
   };
 
   return (
-    <div className="pt-16 flex ">
-      <div className="min-h-[100%] w-[50%] py-32">
+    <div className="flex relative">
+      <NavLink to={'/'} className="absolute top-10 left-10 p-2 border rounded-full hover:border-[#12143d] duration-300">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+  <path fillRule="evenodd" d="M7.28 7.72a.75.75 0 0 1 0 1.06l-2.47 2.47H21a.75.75 0 0 1 0 1.5H4.81l2.47 2.47a.75.75 0 1 1-1.06 1.06l-3.75-3.75a.75.75 0 0 1 0-1.06l3.75-3.75a.75.75 0 0 1 1.06 0Z" clipRule="evenodd" />
+</svg>
+
+      </NavLink>
+      <div className="flex justify-center items-center w-[50%] bg-gray-100">
+      <div className="min-h-[100vh] w-[50%] flex justify-center items-center">
         <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
           <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
             Welcome to GameSpace
@@ -108,10 +116,12 @@ const Login: React.FC = () => {
           <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">Dont have an Account? <NavLink to='/register' className="text-blue-500 underline">Refister</NavLink></p>
         </div>
       </div>
+      </div>
 
-      <div className="w-[50%] bg-[#12143d] text-white flex justify-center items-center pt-16">
-        <div className="flex justify-between items-center flex-col gap-5 w">
-          <img src={login} alt="" className="w-[60%] xl:w-[50%]" />
+      <div className="w-[50%] bg-[#12143d] text-white hidden md:flex justify-center items-center pt-16 ">
+        <div className="flex justify-between items-center flex-col gap-5 bg-white p-8 rounded-md">
+          
+          <Lottie animationData={login} loop={true} />
         </div>
       </div>
     </div>
